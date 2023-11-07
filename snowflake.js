f515515507d2dee180e65d4f188c68f69368c6f3
NineChips <115571967+NineChips@users.noreply.github.com>
2023-11-07 12:54:51 -0700

    Update snowflake.js

--- snowflake.js
+++ snowflake.js
@@ -1,7 +1,22 @@
+var countDownDate = new Date("Dec 25, 2023 00:00:00").getTime();
 
-  for (var i = 0; i < 50; i++) {
-    var snowflake = document.createElement("div");
-    snowflake.className = "snowflake";
-    document.body.appendChild(snowflake);
-  }
-
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                
+    document.getElementById("days").innerHTML = days;
+                
+          document.getElementById("hrs").innerHTML = hours;
+                
+                document.getElementById("mins").innerHTML = minutes;
+                
+                document.getElementById("secs").innerHTML = seconds;
+}, 1000);
